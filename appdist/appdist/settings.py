8d21f4b2c4b314a0d9337410047cb802558e7a9f
sonkm3 <sonkm3@gmail.com>
2015-03-30 12:48:49 +0900

    publicのURL上パスを修正

--- appdist/appdist/settings.py
+++ appdist/appdist/settings.py
@@ -81,9 +81,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/public/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    # os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'public'),
 )
 STATICFILES_FINDERS = (
