bfe1f3641a9ab4ee29c3b2a85d79b9a144814112
sonkm3 <sonkm3@gmail.com>
2015-03-30 12:35:29 +0900

    ホスト名取得を変更

--- appdist/application/views.py
+++ appdist/application/views.py
@@ -3,15 +3,17 @@ from django.shortcuts import render, redirect
 from .models import DistApp
 
 
+def get_server_name(request):
+    return request.META['HTTP_HOST']
+
 def root_view(request):
     return redirect('/list/')
 
-
 def list_view(request):
     app_list = DistApp.objects.filter(display=True)
-    return render(request, 'list.html', {'app_list': app_list, 'server_name': request.META['SERVER_NAME']})
+    return render(request, 'list.html', {'app_list': app_list, 'server_name': get_server_name(request)})
 
 def plistgen_view(request, app_id, updated):
     app = DistApp.objects.get(pk=app_id)
-    return render(request, 'app.plist', {'app': app, 'server_name': request.META['SERVER_NAME']}, content_type='application/octet-stream')
+    return render(request, 'app.plist', {'app': app, 'server_name': get_server_name(request)}, content_type='application/octet-stream')
 
